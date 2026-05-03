cb0c879f1ff4eac3288bacf97d383f5ee20578a9
Sanjeev Midda <s.midda1@gmail.com>
2026-05-03 11:54:10 +0100

    feat: created component for counter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import "./index.css";
 
+// the user can increase/decrease the counter
+// dumb component
+// utility function which holds the logic to increase/decrease
+
 function App() {
   return <div className="App"></div>;
 }
---
+++ src/components/Counter.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Counter = () => {
+  return (
+    <div className="counter">
+      <button id="increase">I</button>
+      <p>{}</p>
+      <button id="decrease">D</button>
+    </div>
+  );
+};
+
+export default Counter;
