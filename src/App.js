1422376882e12f7f567847e7e8c3487704c235a4
Sanjeev Midda <s.midda1@gmail.com>
2026-05-03 11:57:34 +0100

    style: added basic styles for App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./index.css";
+import Counter from "./components/Counter";
 
 // the user can increase/decrease the counter
 // dumb component
 // utility function which holds the logic to increase/decrease
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
 }
 
 export default App;
