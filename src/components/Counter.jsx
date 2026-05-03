6580b383cf5b934208392b241284b5817cd6741e
Sanjeev Midda <s.midda1@gmail.com>
2026-05-03 11:55:38 +0100

    feat: added state to track counter value in COunter component

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="counter">
       <button id="increase">I</button>
-      <p>{}</p>
+      <p>{counter}</p>
       <button id="decrease">D</button>
     </div>
   );
